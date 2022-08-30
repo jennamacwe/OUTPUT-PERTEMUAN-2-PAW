e79f35d6fe18cf34794dd6c45f9e12dac119c1ce
jennamacwe <107898237+jennamacwe@users.noreply.github.com>
2022-08-30 20:08:15 +0700

    Add files via upload

--- output2.php
+++ output2.php
@@ -3,8 +3,11 @@
 
     function volbalok($panjang, $lebar, $tinggi){
         $x = $panjang * $lebar * $tinggi ; 
-        echo "volume dari balok yang berukuran panjang ".$panjang."cm, lebar ".$lebar."cm dan tinggi ".$tinggi."cm adalah: <b>".$x." cm**3 </b>";
+        echo "volume dari balok yang berukuran panjang ".$panjang."cm, lebar ".$lebar."cm dan tinggi ".$tinggi."cm adalah: <b>".$x." cm**3 </b> <br><br>";
     }
 
     volbalok(15, 5, 7);
+    volbalok(5, 5, 17);
+    volbalok(8, 15, 7);
+
 ?>
